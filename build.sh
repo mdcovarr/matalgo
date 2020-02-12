45f305857ab4b591a77ecb04d391a73b1ea17d7c
Michael C <mdcovarrubias@gmail.com>
2020-02-12 10:11:34 -0800

    Add basic template to the build.sh script for deploy and development

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+handle_error() {
+    echo -e "Error: Unhandled argument received, exiting..."
+}
+
+handle_version() {
+    echo -e "version 1.0"
+}
+
 handle_help() {
     echo -e "Usage: build.sh [TYPE]\n"
     echo -e "build.sh - Build Matrix Multiplication &  Transposition Library\n"
@@ -11,6 +19,44 @@ handle_help() {
 
     return $1
 }
+
+handle_deploy() {
+
+}
+
+handle_develop() {
+
+}
+
+for elem in "$@"
+do
+    if [ "$elem" = "--help" -o "$elem" = "-h" ]
+    then
+        handle_help 0
+        exit 0
+    fi;
+
+    if [ "$elem" = "--version" -o "$elem" = "-v" ]
+    then
+        handle_version
+        exit 0
+    fi;
+
+    if [ "$elem" = "deploy" ]
+    then
+        handle_deploy
+        exit $?
+    fi;
+
+    if [ "$elem" = "develop" ]
+    then
+        handle_develop
+        exit $?
+    fi;
+
+    handle_error
+    exit 1
+done
 
 rm -rf build
 mkdir build
