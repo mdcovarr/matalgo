f80b9e33b909779c48ccab3d6029d3358acd1489
Michael C <mdcovarrubias@gmail.com>
2020-02-29 14:50:25 -0800

    Add documentation to MatrixException class

--- utils/MatrixError.hpp
+++ utils/MatrixError.hpp
@@ -13,7 +13,11 @@ static const std::string ERROR_1 = "Index out of range";
 static const std::string ERROR_2 = "Incompatible Matrix Dimensions";
 static const std::string ERROR_3 = "Invalid dimension for Matrix object";
 static const std::string ERROR_4 = "Not enough data in Matrix object";
+static const int NUMBER_OF_ERRORS = 4;
 
+/**
+ * Matrix Exception Handling Helper class.
+ */
 class MatrixError {
     private:
         std::string message;
@@ -21,27 +25,37 @@ class MatrixError {
 
     public:
 
+        /**
+         * Constructing Matrix Error object
+         * @param code - integer value of error code occuring
+         * @param errorMessage - error message string
+         */
         MatrixError(int code, std::string errorMessage) {
-            // need to get error code
-            errorCode = code;
+            // currently only have 4 different error codes
             std::stringstream ss(errorMessage);
 
-            if (errorCode < 1) {
+            if (code < 1 || code > NUMBER_OF_ERRORS) {
                 errorCode = -1;
-            }
-
-            if (errorCode < 1) {
                 message = "This is an unknown exception message";
             } else {
+                errorCode = code;
                 message = errorMessage;
             }
         }
 
 
+        /**
+         * Function used to get error message when throwing exception
+         * @return message - string representation of error
+         */
         inline std::string getErrorMessage() {
             return message;
         }
 
+        /**
+         * Function used to get error code when throwing exception
+         * @return errorCode - errorCode integer of error that occurred
+         */
         inline int getErrorCode() {
             return errorCode;
         }
