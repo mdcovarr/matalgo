85998b352e8f16e6d11549fc22b85f0b8c2d3408
Michael C <mdcovarrubias@gmail.com>
2020-02-28 16:31:26 -0800

    Check if transpose is working accordingly

--- MatrixAlgo.cpp
+++ MatrixAlgo.cpp
@@ -22,6 +22,7 @@ int main(int argc, char* argv[]) {
     Matrix<int> a(ARRAY_SIZE(am), ARRAY_SIZE(am[0]), am[0], ARRAY_SIZE(am)*ARRAY_SIZE(am[0]));
     Matrix<int> b(ARRAY_SIZE(bm), ARRAY_SIZE(bm[0]), bm[0], ARRAY_SIZE(bm)*ARRAY_SIZE(bm[0]));
     Matrix<int> c;
+    Matrix<int> d;
 
     std::cout << a.size() << std::endl;
 
@@ -29,6 +30,7 @@ int main(int argc, char* argv[]) {
 
     try {
         c = a * b;
+
         for (unsigned int i = 0; i < c.rowNum(); i++) {
             for (unsigned int j = 0; j < c.colNum(); j++) {
                 std::cout <<  c[i][j] << "  ";
@@ -37,6 +39,22 @@ int main(int argc, char* argv[]) {
         }
     } catch (MatrixError& e) {
         std::cerr << e.getErrorMessage() << std::endl;
+
+        return e.getErrorCode();
+    }
+
+    try {
+        d = c.transpose();
+
+        for (unsigned int i = 0; i < d.rowNum(); i++) {
+            for (unsigned int j = 0; j < d.colNum(); j++) {
+                std::cout << d[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    } catch (MatrixError& e) {
+        std::cerr << e.getErrorMessage() << std::endl;
+
         return e.getErrorCode();
     }
 }
